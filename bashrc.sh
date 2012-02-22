8088215b1fa79d9851e0361b1e1a442ff139fbe3
Pavel Zhytomirsky <pavelz@netvertise.co.il>
2012-02-22 12:01:42 +0200

    Work changes

--- bashrc.sh
+++ bashrc.sh
@@ -1,21 +1,20 @@
-# Alias definitions.
-# You may want to put all your additions into a separate file like
-# ~/.bash_aliases, instead of adding them here directly.
-# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+# Editor
+export EDITOR='/usr/bin/vim'
 
-if [ -f ~/.r3volut1oner/bash_aliases ]; then
-    . ~/.r3volut1oner/bash_aliases.sh
-fi
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-#if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-#    . /etc/bash_completion
-#fi
-
-# PS1
-
+# Simple PS1
 PS1="\[\e[1;34m\][\u@\h]\[\e[m\] \[\e[1;32m\]\w \[\e[1;37m\]\$ \[\e[m\]"
 
-# setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll us,ru,il
+# Alias definitions.
+if [ -f ~/.r3volut1oner/bash/aliases ]; then
+    . ~/.r3volut1oner/bash/aliases.sh
+fi
+
+# Load svn extend
+if [ -f ~/.r3volut1oner/bash/svn_extend.sh ]; then
+    . ~/.r3volut1oner/bash/svn_extend.sh
+fi
+
+# Load git terminal extend
+if [ -f ~/.r3volut1oner/bash/git_ps1.sh ]; then
+    . ~/.r3volut1oner/bash/git_ps1.sh
+fi
--- vimrc.vim
+++ vimrc.vim
@@ -1,10 +1,16 @@
 " Auto load modules. module autoload/pathogen.vim
-call pathogen#infect()
+call pathogen#infect('bundle')
 syntax on
 filetype plugin indent on
 
 " Load mouse
 set mouse=a
+set mousemodel=popup
+
+" NERDTree Hot Keys
+nmap <F3> <Esc>:NERDTree<cr>
+vmap <F3> <Esc>:NERDTree<cr>
+imap <F3> <Esc>:NERDTree<cr>
 
 " Set line numbers
 set nu
@@ -12,5 +18,7 @@ set nu
 set t_Co=256
 
 " Tabs config
+set smartindent
 set tabstop=4
 set shiftwidth=4
+set expandtab
